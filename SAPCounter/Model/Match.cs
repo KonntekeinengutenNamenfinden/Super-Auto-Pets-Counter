616354e434c9cfb6f3f63cd82cb786893024d502
Toni Gräser <toni.graeser@softland.de>
2022-10-06 10:11:01 +0200

    Projektdateien hinzufügen.

---
+++ SAPCounter/Model/Match.cs
@@ -0,0 +1,13 @@
+﻿namespace SAPCounter.Model
+{
+    public class Match
+    {
+        public enum SapMatchType
+        {
+            Summoner,
+            Hybrid,
+            Other
+        }
+        public SapMatchType CurrentMatchType { get; set; }
+    }
+}
---
+++ SAPCounter/ViewModel/ViewModelMatch.cs
@@ -0,0 +1,18 @@
+﻿using SAPCounter.Model;
+using System.Collections.Generic;
+
+namespace SAPCounter.ViewModel
+{
+    public class ViewModelMatch
+    {
+        public List<Match> AllMatches { get; set; } = new();
+
+        public ViewModelMatch(Match.SapMatchType type)
+        {
+            AllMatches.Add(new Match
+            {
+                CurrentMatchType = type
+            });
+        }
+    }
+}
